6caad165feb3635160469a363a26b8f2f1e772ce
kali <kali@kali.edu>
2023-05-05 20:05:21 -0500

    07 Done

--- easy6/07-reverse-it.js
+++ easy6/07-reverse-it.js
@@ -6,11 +6,47 @@ reverseSentence('Reverse these words');    // "words these Reverse"
 // first thing that needs to be done is make sure to split the string into an array after you have as array iterate over the array and make sure to send it back into and empty string that will catch it 
 // basically iterate backward through the string easy peasy
 
-const reverseSentence = (str) => {
-	let arr = str.split(" ");
-	console.log(arr);
-}
+// more code to combine this string 
+// now lets do this with less code 
 
-reverseSentence("")
-reverseSentence("Hello World") 
-reverseSentence("Reverse these Words")
+const reverseSentence = (str) => {
+	let result = "";
+	let arr = str.split(" ");
+	let arrL = arr.length
+	if(str.length > 0) {
+		for( let z = 0; z < arrL; z++) {
+			result += arr.pop() + " ";
+		}
+	}
+	else{
+		return result;
+	}
+	let s = result.split(" ")
+	s.pop()
+	
+	let t = s.join(" ")
+	return t
+}
+console.log("First Solution ")
+console.log(reverseSentence('')==="")
+console.log(reverseSentence('Hello World')==="World Hello")
+console.log(reverseSentence('Reverse these words')==="words these Reverse")
+
+const reverseSentence2 = (str) => {
+	let result = [];
+	let arr = str.split(" ")
+
+	for( let i = arr.length -1; i >= 0; i--) {
+		result.push(arr[i])
+	}
+	return result.join(" ")
+}
+console.log("Second Solution ") 
+console.log(reverseSentence('')==="")
+console.log(reverseSentence2('Hello World')==="World Hello")
+console.log(reverseSentence2('Reverse these words')=== "words these Reverse")
+
+
+
+
+
