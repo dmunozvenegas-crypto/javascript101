408e86d0e658ab8f89c4476a9ac2169b52ce8e58
kali <kali@kali.edu>
2023-05-05 21:50:56 -0500

    08 working on it

---
+++ easy6/08-reverse-it.js
@@ -0,0 +1,54 @@
+/*Write a function that takes a string argument containing 
+ * one or more words and returns a new string containing 
+ * the words from the string argument. All five-or-more letter 
+ * words should have their letters in reverse order. The string 
+ * argument will consist of only letters and spaces. Words will 
+ * be separated by a single space.
+reverseWords('Professional');             // "lanoisseforP"
+reversewords('walk around the block');    // "walk dnuora the kcolb"
+reverseWords('Launch School');            // "hcnuaL loohcS"
+ */
+// First we must split the string into an array. Once an array then get the length of each individual element if the 
+// element is greater or equal to 5 then we must iterate over the word itself and save it into place holder that will be sent back into  an empty array by pushing it into it 
+
+
+const reverseWords = (word) => {
+	let result = []; 
+	let arr = word.split(" ")
+	console.log("logging the first split of the word")
+	console.log(arr)
+	for( let i = 0; i < arr.length; i++){
+		let cI = arr[i];
+		console.log("current index")
+		console.log(cI)
+		let s = "";
+		console.log("Loggin empty string\n " + s)
+		if(cI.length >4) {
+			for(let x = cI.length -1; x >= 0; x--) {
+				s += cI[x];
+							}
+		}
+		else{
+			result.push(cI)
+		}
+		result.push(s)
+	}
+	
+	return result.join(" ");
+}
+
+//console.log(reverseWords('Professional')==="lanoisseforP")
+
+
+//console.log(reverseWords('Launch School')==="hcnuaL loohcS")
+console.log(reverseWords('walk around the block')==="walk dnuora the kcolb")
+
+
+/*
+console.log("First\n")
+reverseWords('Professional');     
+console.log("second\n")
+reverseWords('Walk around the block');    
+console.log("third\n")
+reverseWords('Launch School');   
+*/
