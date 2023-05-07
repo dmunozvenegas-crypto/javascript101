d773f7f89e5ca85c65354f1d6c239dff1b80644a
kali <kali@kali.edu>
2023-05-07 17:40:30 -0500

    Started ten just needs last check

--- easy6/10-matching-parentheses.js
+++ easy6/10-matching-parentheses.js
@@ -8,3 +8,58 @@ console.log(isBalanced("Hey!") === true);
 console.log(isBalanced(")Hey!(") === false);
 console.log(isBalanced("What ((is))) up(") === false); 
 */
+
+// need to run a loop that checks if the number of paranthesis is the same make sure to have counters for both parenthesis you can easily do this with having two counters that compares them 
+// if they are the same then make sure to return true or else return false 
+// must have a way to catpure the current sequence of par
+// if not catch in the right order must return false otherwise it is true 
+const isBalanced = (str) => {
+	let arr = str.split("")
+	let front = 0;
+	let back = 0;
+	let flag = false;
+	let seq = ""
+	// empty array will be in charge of catching 
+	// current order then we need to be able to retrieve it and compare it to
+	// what the order needs to be else must return false and fail 
+	let result =[];
+
+	for( let x = 0; x < arr.length; x++ ) {
+		if(arr[x] === "(" ) {
+			front += 1;
+			seq += arr[x]
+		}
+		else if( arr[x] === ")") {
+			back += 1;
+			seq += arr[x]
+		}
+	}
+	console.log("Front: \n" + front )
+	console.log("Back: \n" + back ) 
+	if(front === back) {
+		flag = true;
+	}
+	else{
+		flag = false;
+	}
+        console.log(seq)
+	return flag;
+}
+console.log(isBalanced("What (is) this?") === true);
+console.log("**********************************")
+console.log(isBalanced("What is) this?") === false);
+console.log("**********************************")
+console.log(isBalanced("What (is this?") === false);
+console.log("**********************************")
+console.log(isBalanced("((What) (is this))?") === true);
+console.log("**********************************")
+console.log(isBalanced("((What)) (is this))?") === false);
+console.log("**********************************")
+console.log(isBalanced("Hey!") === true);
+console.log("**********************************")
+// last test cases need to check for the seq
+console.log(isBalanced(")Hey!(") === false);
+
+console.log("**********************************")
+console.log(isBalanced("What ((is))) up(") === false); 
+
